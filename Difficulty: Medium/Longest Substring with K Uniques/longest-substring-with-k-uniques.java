b6c0f63344c645c10cc31f6793a44c54c5be1818
Banisha Dhawan <144526688+banishadhawan@users.noreply.github.com>
2026-03-05 23:33:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Substring with K Uniques/longest-substring-with-k-uniques.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public int longestKSubstr(String s, int k) {
+        Map<Character, Integer> map = new HashMap<>();
+        
+        int left = 0;
+        int maxLen = -1;
+
+        for(int right = 0; right < s.length(); right++){
+            char ch = s.charAt(right);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+
+            while(map.size() > k){
+                char leftChar = s.charAt(left);
+                map.put(leftChar, map.get(leftChar) - 1);
+
+                if(map.get(leftChar) == 0){
+                    map.remove(leftChar);
+                }
+                left++;
+            }
+
+            if(map.size() == k){
+                maxLen = Math.max(maxLen, right - left + 1);
+            }
+        }
+
+        return maxLen;
+    }
+}
