773f2f2d7820247237ad0478c77e7d34bc72127d
Banisha Dhawan <144526688+banishadhawan@users.noreply.github.com>
2026-04-05 20:54:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Target Sum/target-sum.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int totalWays(int[] arr, int target) {
+        int sum = 0;
+        
+        for (int num : arr) {
+            sum += num;
+        }
+
+        // Edge cases
+        if ((sum + target) % 2 != 0 || sum < Math.abs(target)) {
+            return 0;
+        }
+
+        int newTarget = (sum + target) / 2;
+
+        // DP array
+        int[] dp = new int[newTarget + 1];
+        dp[0] = 1; // one way to make sum 0
+
+        for (int num : arr) {
+            for (int j = newTarget; j >= num; j--) {
+                dp[j] += dp[j - num];
+            }
+        }
+
+        return dp[newTarget];
+    }
+}
