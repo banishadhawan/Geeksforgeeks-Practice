58dfe9891d1349a581ced411c6e9a53874bcb308
Banisha Dhawan <144526688+banishadhawan@users.noreply.github.com>
2026-04-16 22:05:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Implement Atoi/implement-atoi.java
+++ Difficulty: Medium/Implement Atoi/implement-atoi.java
@@ -1,33 +1,33 @@
 class Solution {
     public int myAtoi(String s) {
         // code here
-        int n = s.length();
-        int i=0, ans=0;
-        int sign=1;
-        
-        //1. skip space
-        while(i<n && s.charAt(i)==' ') i++;   
-        
-        //2. sign
-        if(i<n && (s.charAt(i)=='+' || s.charAt(i)=='-')){
-            sign = (s.charAt(i) =='-')?-1:1;
-            i++;
-        }
-        
-        //3. digits conversion
-       while(i<n && Character.isDigit(s.charAt(i))){
+       int n=s.length();
+       int i=0;
+       int sign=1,ans=0;
+       
+       while(i<n && s.charAt(i)==' ') i++;
+       
+       if(i<n && (s.charAt(i)=='-' || s.charAt(i)=='+')){
+           sign = (s.charAt(i)=='-') ?-1:1;
+           i++;
+       }
+       
+       while(i<n && s.charAt(i)>='0' && s.charAt(i)<='9'){
            int digit = s.charAt(i)-'0';
            
-           //4. overflow handle
-           if(ans > (Integer.MAX_VALUE-digit)/10){
+           if(ans>(Integer.MAX_VALUE-digit)/10){
                return (sign==1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
            }
            
            ans = ans*10 + digit;
            i++;
-       } 
-        return sign*ans;
+       }
+       
+       return sign*ans;
+       
     }
             
        
 }
+
+      
