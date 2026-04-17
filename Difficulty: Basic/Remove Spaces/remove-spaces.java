1c0d6e49a77e7939e00d0ab9a7db41ab04b3043a
Banisha Dhawan <144526688+banishadhawan@users.noreply.github.com>
2026-04-17 21:45:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Remove Spaces/remove-spaces.java
+++ Difficulty: Basic/Remove Spaces/remove-spaces.java
@@ -1,11 +1,13 @@
 class Solution {
     String removeSpaces(String s) {
-        String result = "";
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) != ' ') {
-                result += s.charAt(i);
-            }
-        }
-        return result;
+        // String result = "";
+        // for (int i = 0; i < s.length(); i++) {
+        //     if (s.charAt(i) != ' ') {
+        //         result += s.charAt(i);
+        //     }
+        // }
+        // return result;
+        
+        return s.replace(" ", "");
     }
 }
