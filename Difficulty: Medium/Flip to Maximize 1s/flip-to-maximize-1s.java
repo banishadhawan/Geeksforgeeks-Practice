88567a1b989d2b72bed83b93703a00829af4570c
Banisha Dhawan <144526688+banishadhawan@users.noreply.github.com>
2026-04-21 23:18:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Flip to Maximize 1s/flip-to-maximize-1s.java
@@ -0,0 +1,22 @@
+class Solution {
+    int maxOnes(int[] arr) {
+        int totalOnes = 0;
+        
+       
+        for(int num : arr) {
+            if(num == 1) totalOnes++;
+        }
+        
+     
+        int maxGain = 0, currGain = 0;
+        
+        for(int num : arr) {
+            int val = (num == 0) ? 1 : -1;
+            
+            currGain = Math.max(val, currGain + val);
+            maxGain = Math.max(maxGain, currGain);
+        }
+        
+        return totalOnes + maxGain;
+    }
+}
